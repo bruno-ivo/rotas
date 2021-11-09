ad6352b2d02b9045e19ed64a2241e50f4a12ff77
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-09 11:24:40 -0300

    Crição do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterializeButtonModule, MaterializeCardModule } from 'materialize-angular';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeButtonModule,
+    MaterializeCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
